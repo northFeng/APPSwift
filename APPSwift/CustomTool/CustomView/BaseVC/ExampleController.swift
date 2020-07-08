5eed3efd2ec089ba5ceba83e4ba21d62e66f2dbc
峰 <north_feng@sina.cn>
2020-07-08 20:30:02 +0800

    完善优化

---
+++ APPSwift/CustomTool/CustomView/BaseVC/ExampleController.swift
@@ -0,0 +1,50 @@
+//
+//  ExampleController.swift
+//  APPSwift
+//
+//  Created by 峰 on 2020/7/8.
+//  Copyright © 2020 north_feng. All rights reserved.
+//
+
+import Foundation
+
+
+class ExampleController: APPBaseController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.createView()
+        self.bindViewModel()
+    }
+    
+    ///数据计算
+    override func initData() {
+        
+    }
+    
+    //设置状态栏
+    override func setNaviBarStyle() {
+        
+    }
+    
+    //MARK: ************************* viewModel绑定 *************************
+    ///数据绑定
+    func bindViewModel() {
+        
+    }
+    
+    //MARK: ************************* Action && Event *************************
+    
+    
+    //MARK: ************************* 逻辑处理 *************************
+    
+    
+    //MARK: ************************* tableViewDelegate *************************
+    
+    
+    //MARK: ************************* 页面搭建 *************************
+    func createView() {
+        
+    }
+}
--- APPSwift/ViewController/Home/HomeVC.swift
+++ APPSwift/ViewController/Home/HomeVC.swift
@@ -8,48 +8,118 @@
 
 import UIKit
 
-class HomeVC: APPBaseController {
+class HomeVC: APPBaseController, UITableViewDelegate,UITableViewDataSource {
     
     var array1 = [1,2,3,4,5]
     
     var array2 = ["a","b","c","d","f"]
     
+    let tableView = UITableView(frame: CG_Rect(0, 100, kAPPWidth, kAPPHeight - 100), style: .grouped)
     
+    let con = UIRefreshControl(frame: CG_Rect(0, -100, kAPPWidth, 100))
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-        self.view.backgroundColor = UIColor.white
         
-        
-        let oneView = UIView()
-        oneView.backgroundColor = UIColor.red
-        self.view.addSubview(oneView)
-        
-        oneView.snp.makeConstraints { (make) in
-            make.left.equalToSuperview().offset(100)
-            make.top.equalToSuperview().offset(150)
-            make.width.height.equalTo(100)
-        }
-        
-        let twoView = UIView()
-        twoView.backgroundColor = UIColor.green
-        self.view.addSubview(twoView)
-        
-        twoView.snp.makeConstraints { (make) in
-            make.left.equalTo(oneView.snp.right)
-            make.top.equalToSuperview().offset(150)
-            make.width.height.equalTo(oneView).multipliedBy(2)
-        }
-        
-        APPCache.setString(text: "点击的京东到家", key: "ffff")
-                    
+        self.createView()
+        self.bindViewModel()
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    ///数据计算
+    override func initData() {
         
-
     }
+    
+    //设置状态栏
+    override func setNaviBarStyle() {
+        
+    }
+    
+    //MARK: ************************* viewModel绑定 *************************
+    ///数据绑定
+    func bindViewModel() {
+        
+    }
+    
+    //MARK: ************************* Action && Event *************************
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        con.beginRefreshing()
+        
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2) {
+            self.con.endRefreshing()
+        }
+    }
+    
+    
+    
+    //MARK: ************************* 逻辑处理 *************************
+    
+    
+    //MARK: ************************* tableViewDelegate *************************
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3//返回组数
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10//返回行数
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //返回 cell
+        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = "第\(String(indexPath.section))组的\(indexPath.row)行" 
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return nil//返回组头视图
+    }
+
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return nil//返回组尾视图
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 0.1//返回组头高度
+    }
+
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 20//返回组尾高度
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 40//返回cell高度
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //点击cell
+    }
+    
+
+    //MARK: ************************* 页面搭建 *************************
+    func createView() {
+        
+        tableView.backgroundColor = UIColor.gray
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.sectionHeaderHeight = 0
+        tableView.sectionFooterHeight = 0
+        tableView.estimatedRowHeight = 0
+        tableView.estimatedSectionHeaderHeight = 0
+        tableView.estimatedSectionFooterHeight = 0
+        tableView.separatorStyle = .none
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        //tableView.register(UINib(nibName: "", bundle: nil), forCellReuseIdentifier: "cell")
+        self.view.addSubview(tableView)
+        
+        con.tintColor = UIColor.red
+        con.attributedTitle = NSAttributedString(string: "刷新")
+        tableView.addSubview(con)
+    }
+    
     
 }
 
